f865730b07c8d5e8c3ba1a652715fffa240882d2
unknown <mohamed_mahmoud_mmm@yahoo.com>
2019-07-02 12:45:26 +0200

    khood

--- App.js
+++ App.js
@@ -23,6 +23,8 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>ya team 3eeeeeel2</Text>
+
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
